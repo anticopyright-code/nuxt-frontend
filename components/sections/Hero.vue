e5b575c354937c24ab1ed3f375038f80af94df8e
Anticopyright <53624153+dudul-bdul@users.noreply.github.com>
2022-11-03 18:14:13 +0700

    new

---
+++ components/sections/Hero.vue
@@ -0,0 +1,77 @@
+<template>
+  <section id="hero">
+    <v-carousel
+      height="calc(100vh - 64px)"
+      dark
+      cycle
+      show-arrows-on-hover
+      draggable="true"
+      hide-delimiter-background
+    >
+      <v-carousel-item
+        v-for="(carousel, carindex) in carouselsData"
+        :key="carindex"
+        :src="carousel.src"
+        class="gradient-fill"
+      >
+        <v-container fill-height>
+          <div style="max-width: 600px">
+            <div
+              class="text-md-h2 mb-3 text-sm-h3 text-h5 font-weight-black white--text"
+            >
+              {{ carousel.heading.toUpperCase() }}
+            </div>
+            <p class="mb-5 white--text">{{ carousel.subHeading }}</p>
+            <v-btn :x-large="$vuetify.breakpoint.smAndUp" class="my-3 primary"
+              ><v-icon left large color="accent">mdi-account-reactivate</v-icon>
+              Daftar</v-btn
+            >
+            <span class="mx-2 my-4"></span>
+            <v-btn
+              :x-large="$vuetify.breakpoint.smAndUp"
+              text
+              class="my-3"
+              outlined
+              dark
+              ><v-icon left large color="primary">mdi-instagram</v-icon
+              >Instagram
+            </v-btn>
+          </div>
+        </v-container>
+      </v-carousel-item>
+    </v-carousel>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      carouselsData: [
+        {
+          src: 'hero-1.jpg',
+          heading: ' YELLOW EARTH ',
+          subHeading: 'ENVIRONMENTAL DIGITAL MOVEMENT',
+        },
+        {
+          src: 'yellow-earth-PAGE-PHONE4.webp',
+          heading: ' YELLOW EARTH',
+          subHeading:
+            'Yellow Earth membuat kita bersama-sama menyelamatkan Bumi dengan cara yang menyenangkan. Kami membuat semua-nya seperti permainan..',
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style>
+.gradient-fill .v-responsive__content {
+  background: rgb(0, 0, 0);
+  background: linear-gradient(
+    to right,
+    rgba(3, 12, 41, 0.75),
+    rgba(5, 11, 31, 0.65)
+  );
+}
+</style>
