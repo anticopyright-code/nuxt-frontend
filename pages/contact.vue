23dd720bf1869ca060b652403c9fc565a08bfb07
Anticopyright <53624153+dudul-bdul@users.noreply.github.com>
2022-11-03 18:25:25 +0700

    Update contact.vue

--- pages/contact.vue
+++ pages/contact.vue
@@ -16,7 +16,7 @@
                   <v-list-item-content>
                     <v-list-item-title>ADDRESS</v-list-item-title>
                     <v-list-item-subtitle
-                      >1400 Main Street, Orlando, FL 79938</v-list-item-subtitle
+                      >KST Unpad, Bandung, Indonesia</v-list-item-subtitle
                     >
                   </v-list-item-content>
                 </v-list-item>
@@ -95,7 +95,7 @@
           </v-row>
         </v-container>
         <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3505.285388316138!2d-81.37969424867693!3d28.531140482371708!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88e77b041135b063%3A0x13aab64f9cc768fc!2s1400%20Main%20Ln%2C%20Orlando%2C%20FL%2032801%2C%20USA!5e0!3m2!1sen!2sin!4v1606230494596!5m2!1sen!2sin"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3960.763040674982!2d107.76854441435749!3d-6.918907269640867!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e68db4bb59c6e37%3A0x39209981dea59649!2sGedung%20Kawasan%20Sains%20dan%20Teknologi%20(KST)%20-%20Unpad!5e0!3m2!1sid!2sid!4v1667474557890!5m2!1sid!2sid"
           width="100%"
           height="600"
           frameborder="0"
